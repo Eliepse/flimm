844f470ca9ed7321bbc075ff6e515d1a14c43aa0
Élie Meignan <contact@eliepse.fr>
2021-06-23 20:24:38 +0200

    feat: add logout

--- resources/js/components/layouts/DashboardLayout.js
+++ resources/js/components/layouts/DashboardLayout.js
@@ -1,6 +1,10 @@
 import {Navigation} from 'hds-react';
+import {useContext} from 'react';
+import {authContext} from '../../lib/auth/authProvider';
 
 export default function DashboardLayout({children}) {
+	const {user, logout} = useContext(authContext);
+
 	return (
 		<div className="flex flex-col min-h-screen">
 			<Navigation
@@ -12,6 +16,11 @@ export default function DashboardLayout({children}) {
 				<Navigation.Row variant="inline">
 					<Navigation.Item label="Admin" href="/admin" active/>
 				</Navigation.Row>
+				<Navigation.Actions>
+					<Navigation.User authenticated label="Se connecter" userName={user.firstname}>
+						<Navigation.Item label="Se déconnecter" onClick={logout} />
+					</Navigation.User>
+				</Navigation.Actions>
 			</Navigation>
 			<div className="flex-auto w-full pt-6 mx-auto max-w-6xl">{children}</div>
 		</div>
