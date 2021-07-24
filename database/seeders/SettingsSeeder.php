046228fd783b25de35445478ae2c03c78a7dcccc
Élie Meignan <contact@eliepse.fr>
2021-07-24 15:45:52 +0200

    fix: correct parameters

--- database/seeders/SettingsSeeder.php
+++ database/seeders/SettingsSeeder.php
@@ -15,7 +15,8 @@ class SettingsSeeder extends Seeder
 	public function run(SettingRepository $repository)
 	{
 		$repository->createMany([
-			["name" => "socials", "value" => ["facebook" => null, "instagram" => null]],
+			["name" => "socials.facebook", "value" => null],
+			["name" => "socials.instagram", "value" => null],
 			["name" => "header.text", "value" => "21 - 24.10.2021\nDOC - Paris"],
 			["name" => "homepage.featuredImage", "isMedia" => true]]);
 	}
