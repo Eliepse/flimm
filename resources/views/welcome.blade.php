04c9249f6ca48f1fa989b9fe209a3a12e67afd90
Élie Meignan <contact@eliepse.fr>
2021-07-25 11:29:52 +0200

    feat: setup homepage bases

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,24 @@
+<?php
+
+use App\Models\Article;
+use App\Repositories\SettingRepository;
+use Illuminate\Database\Eloquent\Collection;
+
+/**
+ * @var Collection|Article[] $actus
+ * @var SettingRepository $settings
+ */
+?>
+
 @extends("layouts.root-public")
+
+@section("main")
+	@if($featuredImage = $settings->get("homepage.featuredImage"))
+		<header class="border-b-2 border-solid border-black">
+			<img src="{{ $featuredImage->value->getFullUrl() }}" class="mx-auto my-0 object-cover" alt="" />
+		</header>
+	@endif
+	<section>
+		@include("common.actusList", ["articles" => $actus])
+	</section>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\HomepageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'welcome');
+Route::get('/', HomepageController::class);
 Route::view('/admin/{any?}', 'admin')->where("any", ".*");
 Route::get('/actus', [ArticleController::class, "index"])->name("actus");
 Route::get('/actus/{article:slug}', [ArticleController::class, "show"])->name("article");
