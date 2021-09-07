aded3a3d59fa42ad5c082943e261511d2295dbd6
Élie Meignan <contact@eliepse.fr>
2021-09-07 20:29:50 +0200

    change: add socials on header

--- resources/views/common/header.blade.php
+++ resources/views/common/header.blade.php
@@ -1,7 +1,32 @@
+<?php
+
+use App\Repositories\SettingRepository;
+
+/**
+ * @var SettingRepository $settings
+ */
+?>
 <header class="flex py-2 px-6 justify-between border-b-2 border-black leading-tight font-bold">
 	<div>
 		FLiMM<br>
 		Festival Libre du Moyen-Métrage<br>
 		@nl2br(app(\App\Repositories\SettingRepository::class)->get("header.text")?->value)
 	</div>
+	<ul class="flex items-end">
+		@if($settings->isFilled("socials.instagram"))
+			<li>
+				<a href="{{ $settings->get("socials.instagram")->value }}" rel="noreferrer nofollow">
+					<?php include public_path("images/instagram.svg") ?>
+				</a>
+			</li>
+		@endif
+
+		@if($settings->isFilled("socials.facebook"))
+			<li class="ml-4">
+				<a href="{{ $settings->get("socials.facebook")->value }}" rel="noreferrer nofollow">
+					<?php include public_path("images/facebook.svg") ?>
+				</a>
+			</li>
+		@endif
+	</ul>
 </header>
