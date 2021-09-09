cdfd7c31a7705583d894073122bdb22b11eef556
Élie Meignan <contact@eliepse.fr>
2021-09-09 21:16:11 +0200

    change: new navigation bar & dev message

--- resources/js/components/layouts/DashboardLayout.js
+++ resources/js/components/layouts/DashboardLayout.js
@@ -1,28 +1,56 @@
-import { Navigation } from "hds-react";
 import { useContext } from "react";
 import { authContext } from "lib/auth/authProvider";
-import { Link } from "app";
 import PropTypes from "prop-types";
+import { Avatar, Menu } from "antd";
+import { useRouter } from "lib/useRouter";
 
 const DashboardLayout = ({ children }) => {
+	const router = useRouter();
 	const { user, logout } = useContext(authContext);
+
+	function handleNavigationClick(item) {
+		router.pushAdmin(item.key);
+	}
 
 	return (
 		<div className="flex flex-col min-h-screen">
-			<Navigation title="FLiMM" menuToggleAriaLabel="menu" skipTo="#content" skipToContentLabel="Skip to content">
-				<Navigation.Row variant="inline">
-					<Navigation.Item label="Dashboard" as={Link} href="/" />
-					<Navigation.Item label="Articles" as={Link} href="/articles" />
-					<Navigation.Item label="Films" as={Link} href="/films" />
-					<Navigation.Item label="Editions" as={Link} href="/editions" />
-					<Navigation.Item label="Paramètres" as={Link} href="/settings" />
-				</Navigation.Row>
-				<Navigation.Actions>
-					<Navigation.User authenticated label="Se connecter" userName={user.firstname}>
-						<Navigation.Item label="Se déconnecter" onClick={logout} />
-					</Navigation.User>
-				</Navigation.Actions>
-			</Navigation>
+			<div className="border-b border-solid border-gray-200">
+				<div className="max-w-6xl mx-auto flex items-center justify-between">
+					{/* Title */}
+					<div className="font-bold text-xl mr-6">FLiMM</div>
+
+					{/* Menu */}
+					<Menu mode="horizontal" className="border-0" onClick={handleNavigationClick}>
+						<Menu.Item key="/">Dashboard</Menu.Item>
+						<Menu.Item key="/articles">Articles</Menu.Item>
+						<Menu.Item key="/films">Films</Menu.Item>
+						<Menu.Item key="/editions">Editions</Menu.Item>
+						<Menu.Item key="/settings">Paramètres</Menu.Item>
+					</Menu>
+
+					{/* User */}
+					<Menu mode="horizontal">
+						<Menu.SubMenu
+							key="user"
+							icon={
+								<Avatar size={32} shape="square">
+									{user.firstname[0] + user.lastname[0]}
+								</Avatar>
+							}
+							title={`${user.firstname} ${user.lastname[0]}`}
+						>
+							<Menu.Item key="/logout" onClick={logout}>
+								Se déconnecter
+							</Menu.Item>
+						</Menu.SubMenu>
+					</Menu>
+				</div>
+			</div>
+			{/*		<Navigation.Item label="Dashboard" as={Link} href="/" />*/}
+			{/*		<Navigation.Item label="Articles" as={Link} href="/articles" />*/}
+			{/*		<Navigation.Item label="Films" as={Link} href="/films" />*/}
+			{/*		<Navigation.Item label="Editions" as={Link} href="/editions" />*/}
+			{/*		<Navigation.Item label="Paramètres" as={Link} href="/settings" />*/}
 			<div className="flex-auto w-full pt-6 mx-auto max-w-6xl">{children}</div>
 		</div>
 	);
--- resources/js/pages/home.js
+++ resources/js/pages/home.js
@@ -1,5 +1,23 @@
-import DashboardLayout from '../components/layouts/DashboardLayout';
+import DashboardLayout from "../components/layouts/DashboardLayout";
+import { Alert } from "antd";
 
 export default function HomePage() {
-	return <DashboardLayout/>;
+	return (
+		<DashboardLayout>
+			<Alert
+				message="L'administration est toujours en développement."
+				description={
+					<>
+						Si vous rencontrez un problème, vous pouvez{" "}
+						<a className="underline" href="mailto:contact@eliepse.fr">
+							me contacter
+						</a>{" "}
+						pour me l&apos;expliquer. 😊
+					</>
+				}
+				type="info"
+				showIcon
+			/>
+		</DashboardLayout>
+	);
 }
