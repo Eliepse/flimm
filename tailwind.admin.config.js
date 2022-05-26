385e3fbae38b02e3906c99a00be62cdccd6e3406
Élie Meignan <contact@eliepse.fr>
2022-05-26 18:49:17 +0200

    Seperate front compilation for perfs & dev flow

---
+++ tailwind.admin.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	content: ["./resources/scss/admin/**/*.{css,scss}", "./resources/js/**/*.{js,scss}", "./resources/views/**/*.blade.php"],
+	theme: {
+		extend: {},
+	},
+	corePlugins: {
+		preflight: false,
+	},
+	plugins: [],
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
 module.exports = {
-	content: ["./resources/css/**/*.{css,scss}", "./resources/js/**/*.{js,scss}", "./resources/views/**/*.blade.php"],
+	content: ["./resources/scss/public/**/*.{css,scss}", "./resources/views/**/*.blade.php"],
 	theme: {
 		extend: {},
-	},
-	corePlugins: {
-		preflight: false,
 	},
 	plugins: [],
 };
