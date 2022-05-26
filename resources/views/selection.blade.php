9ed56cf4bfc1b9a397448f419ceb9544a466ac28
Élie Meignan <contact@eliepse.fr>
2022-05-26 21:55:41 +0200

    Add breadcrumb

--- resources/views/selection.blade.php
+++ resources/views/selection.blade.php
@@ -1,21 +1,26 @@
 @extends("layouts.root-public")
+<!--suppress CheckEmptyScriptTag -->
 
 <?php
 /**
  * @var \App\Models\Edition $edition
  * @var \App\Models\Selection $selection
  */
+
+$breadcrumbItems = [
+	["title" => $edition->title, "url" => route("edition", $edition)],
+	["title" => $selection->name],
+]
 ?>
-
 @section("main")
-
 	<div class="page-content">
+		<x-breadcrumb :items="$breadcrumbItems" />
+
 		{{-- SELECTION HEADER --}}
 		<h1 class="uppercase mt-8 mb-12">{{ $selection->name }}</h1>
 
 		{{-- FILMS --}}
 		@foreach($selection->films as $film)
-			<!--suppress CheckEmptyScriptTag -->
 			<x-film-card :film="$film" />
 		@endforeach
 	</div>
