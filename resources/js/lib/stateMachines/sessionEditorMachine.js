f4abc8bda644d46ec6baf438fb16bcb58f72b0a7
Élie Meignan <contact@eliepse.fr>
2022-05-29 20:42:02 +0200

    Add visual feedback on error and success

--- resources/js/lib/stateMachines/sessionEditorMachine.js
+++ resources/js/lib/stateMachines/sessionEditorMachine.js
@@ -1,5 +1,6 @@
 import { createMachine, invoke, reduce, state, state as final, transition } from "robot3";
 import apiSession from "lib/api/apiSession";
+import { message } from "antd";
 
 export const MODE = {
 	create: 0,
@@ -15,16 +16,35 @@ function loadSession(ctx) {
 		return Promise.resolve({});
 	}
 
-	return apiSession.get(ctx.id).catch(console.error);
+	return apiSession.get(ctx.id).catch((e) => {
+		console.error(e);
+		message.error("Erreur du chargement de la séance");
+	});
 }
 
 function saveSession(ctx) {
 	console.debug(ctx);
 	if (isCreateMode(ctx)) {
-		return apiSession.create(ctx.dataToSend);
+		return apiSession
+			.create(ctx.dataToSend)
+			.then(() => {
+				message.success("Séance crée");
+			})
+			.catch((e) => {
+				message.error("Échec de création d'une séance");
+				console.error(e);
+			});
 	}
 
-	return apiSession.update({ id: ctx.id, ...ctx.dataToSend });
+	return apiSession
+		.update({ id: ctx.id, ...ctx.dataToSend })
+		.then(() => {
+			message.success("Séance sauvegardée");
+		})
+		.catch((e) => {
+			message.error("Échec de la sauvegarde");
+			console.error(e);
+		});
 }
 
 //noinspection JSCheckFunctionSignatures
