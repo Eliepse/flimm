4ed0e97b180d13fc01c8884c20bb7ae3ae946e1a
Élie Meignan <contact@eliepse.fr>
2024-09-15 16:00:10 +0200

    Fix optimize uses wrong environment

--- update.sh
+++ update.sh
@@ -4,4 +4,5 @@ docker compose build
 docker compose exec -ti app php artisan down
 docker compose up -d
 docker compose exec -ti app php artisan migrate
+docker compose exec -ti app php artisan optimize
 docker compose exec -ti app php artisan up
